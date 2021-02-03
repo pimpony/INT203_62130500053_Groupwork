896cc9a7f468611a1cd58f2b1db4c4f0705517ae
Paweena Thongyan <paweena.thongyan@mail.kmutt.ac.th>
2021-02-03 11:30:56 +0700

    groupwork 2

---
+++ 62130500053_groupwork_2/main.js
@@ -0,0 +1,16 @@
+const profile = {
+    data() {
+        return {
+            name : 'Paweena Thongyan',
+            role : 'Student',
+            show : true,
+            Articles : 0,
+            Followers : 565,
+            Ratings : 3,
+            show : true,
+            img : "./images/cat.png"
+        }
+    }
+
+}
+var mounteApp = Vue.createApp(profile).mount('#profile')
